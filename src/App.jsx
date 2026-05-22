7a8cba018cc37a4f973e72382a120bd18c9b7e5e
Your NameChristian <christianhermanndev@gmail.com>
2026-05-22 12:12:39 -0500

    Basic layout for page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 function App() {
   return (
     <>
-      <h1>Portfolio Page</h1>
+      <main>
+        <section>
+          <p>Hi, my name is Christian.</p>
+
+          <h1>Junior Full Stack Developer</h1>
+
+          <p>
+            I build simple, practical web pps using React, JavaScript, Node, and
+            Express.
+          </p>
+        </section>
+
+        <section>About</section>
+
+        <section>Skills</section>
+
+        <section>Projects</section>
+      </main>
     </>
   );
 }
