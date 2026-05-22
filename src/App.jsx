17678fd4b555f053b7dcbe870b68eff128843bcb
Your NameChristian <christianhermanndev@gmail.com>
2026-05-22 13:35:23 -0500

    Portfolio page structure

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,49 @@ function App() {
           </p>
         </section>
 
-        <section>About</section>
+        <section>
+          <h2>About</h2>
 
-        <section>Skills</section>
+          <p>
+            I recently completed a full-stack coding bootcamp and have been
+            continuing to build projects and strengthen my skills with React,
+            Express, and backend development.
+          </p>
+        </section>
 
-        <section>Projects</section>
+        <section>
+          <h2>Skills</h2>
+
+          <p>JavaScript</p>
+          <p>React</p>
+          <p>Node</p>
+          <p>Express</p>
+          <p>PostgreSQL</p>
+        </section>
+
+        <section>
+          <h2>Projects</h2>
+
+          <h3>FilmRanker</h3>
+          <p>Moive ranking app with authentication and CRUD features.</p>
+
+          <h3>PixelPal</h3>
+          <p>Digital pet applications built as a team project.</p>
+
+          <h3>Todo List</h3>
+          <p>Simple app showing React skills.</p>
+
+          <h3>Sip n' Shred</h3>
+          <p>Current project in development</p>
+        </section>
+
+        <section>
+          <h2>Contact</h2>
+
+          <p>GitHub</p>
+          <p>LinkedIn</p>
+          <p>Email</p>
+        </section>
       </main>
     </>
   );
