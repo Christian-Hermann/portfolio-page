cdfccb92325b8d572b788b90b77b87b6cd004936
Your NameChristian <christianhermanndev@gmail.com>
2026-05-26 17:10:18 -0500

    Updated Projects section

--- src/App.jsx
+++ src/App.jsx
@@ -26,40 +26,53 @@ function App() {
         <section>
           <h2>Skills</h2>
 
-          <p>JavaScript</p>
-          <p>React</p>
-          <p>Node</p>
-          <p>Express</p>
-          <p>PostgreSQL</p>
+          <div className="skills">
+            <p>JavaScript</p>
+            <p>React</p>
+            <p>Node</p>
+            <p>Express</p>
+            <p>PostgreSQL</p>
+          </div>
         </section>
 
-        <section>
+        <section className="projects">
           <h2>Projects</h2>
 
-          <h3>FilmRanker</h3>
-          <p>Moive ranking app with authentication and CRUD features.</p>
+          <div className="project">
+            <h3>FilmRanker</h3>
 
-          <h3>Sip n' Shred</h3>
-          <p>
-            Winter sport and apres-ski application using real APIs and future AI
-            features.
-          </p>
+            <p>Moive ranking app with authentication and CRUD features.</p>
+          </div>
 
-          <h3>PixelPal</h3>
-          <p>
-            Team built digital pet applications built using react and backend
-            APIs.
-          </p>
+          <div className="project">
+            <h3>Sip n' Shred</h3>
 
-          <h3>Todo List</h3>
-          <p>
-            Simple React application for managing and practicing funaamentals.
-          </p>
+            <p>
+              Winter sport and apres-ski application using real APIs and future
+              AI features.
+            </p>
+          </div>
+
+          <div className="project">
+            <h3>PixelPal</h3>
+
+            <p>
+              Team built digital pet applications built using react and backend
+              APIs.
+            </p>
+          </div>
+
+          <div className="project">
+            <h3>Todo List</h3>
+
+            <p>
+              Simple React application for managing and practicing funaamentals.
+            </p>
+          </div>
         </section>
 
         <section>
           <h2>Contact</h2>
-
           <p>GitHub</p>
           <p>LinkedIn</p>
           <p>Email</p>
